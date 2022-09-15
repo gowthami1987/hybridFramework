afd1d55b032d2c5de0b0768ceea4f48cbd21aa46
changes <gowthamisundharaj@gmail.com>
2022-09-15 10:02:06 -0500

    new

--- src/test/java/SnapDeal.java
+++ src/test/java/SnapDeal.java
@@ -1,5 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -10,25 +12,28 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 
+
 public class SnapDeal {
     WebDriver driver = null;
+     Logger log = Logger.getLogger(String.valueOf(SnapDeal.class));
     @Test
     public void Test1() {
+        PropertyConfigurator.configure("/Users/ini/Documents/HybridFramework/src/test/java/objects/log4j.properties");
+
         WebDriverManager.chromedriver().setup();
-       /* DesiredCapabilities cap = new DesiredCapabilities();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-         driver = new ChromeDriver(options);
-        //driver = new ChromeDriver();*/
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+      /*  DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("something", true);
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.merge(capabilities);
         ChromeDriver driver = new ChromeDriver(options);
-        System.out.println("open browser");
+        System.out.println("open browser");*/
+        log.info("open browser");
+        driver=new ChromeDriver();
         driver.get("https://www.gmail.com");
         this.takeSnapShot(driver, System.getProperty("user.dir")+"/"+"test.png") ;
+        driver.close();
 
     }
     public void takeSnapShot(WebDriver webdriver,String fileWithPath) {
